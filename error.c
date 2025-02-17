1b23795248b6c8c36ec6af91c5baf43efdb6738e
mgomes-s <marcellygomes003@gmail.com>
2025-02-17 12:03:51 -0300

    erros

--- error.c
+++ error.c
@@ -6,7 +6,7 @@
 /*   By: mgomes-s <mgomes-s@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/07 09:40:28 by mgomes-s          #+#    #+#             */
-/*   Updated: 2025/02/07 09:47:28 by mgomes-s         ###   ########.fr       */
+/*   Updated: 2025/02/17 11:18:19 by mgomes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@ void	error(int num)
 	if (num == 1)
 		write(2, "error -> no argument passed.\n", 29);
 	if (num == 2)
-		write(2, "error ->\n", 9);
+		write(2, "error -> add more than one argument!\n", 37);
 	if (num == 3)
 		write(2, "error ->\n", 9);
 	if (num == 4)
